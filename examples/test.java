058a67668cf86aa0d2fcb3459fe3741c3dcf92dd
MercuryRiot <gayen.arghya2016@gmail.com>
2026-02-05 21:19:51 +0530

    Hello java file

---
+++ examples/test.java
@@ -0,0 +1,6 @@
+public class HelloExample {
+    public static void main(String[] args) {
+        System.out.println("Hello from the hitproj1 example!");
+        System.out.println("This file was added as part of an open source contribution practice.");
+    }
+}
